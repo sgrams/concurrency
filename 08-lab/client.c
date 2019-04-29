8017ea5e87626191c8f84a77ac576d3b432071fa
Stanisław Grams <sgrams@fmdx.pl>
2019-04-29 21:17:33 +0200

    [08-sockets] add draft 0p3

---
+++ 08-lab/client.c
@@ -0,0 +1,100 @@
+/*
+ * This file is part of 08-sockets task
+ *
+ * client.c
+ * Stanisław Grams <sgrams@fmdx.pl>
+ *
+ */
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+#define DEFAULT_SERVER_ADDR "153.19.1.202"
+#define DEFAULT_SERVER_PORT 5000
+
+// prototypes
+int32_t calculate_y (int32_t x, int32_t *y);
+
+int32_t main (int32_t argc, char *argv[])
+{
+  int32_t status = 0;
+  int32_t x, y;
+
+  // get 4B number pre-allocated on the stack
+  if (argc < 2) {
+    return EXIT_FAILURE;
+  }
+  x = strtol (argv[1], (char **)NULL, 10);
+
+  // send and receive a request to the server
+  status = calculate_y (x, &y);
+  if (-1 == status) {
+    fprintf (stderr, "%s: critical error, terminating...\n", argv[0]);
+    return EXIT_FAILURE;
+  }
+
+  // handle stdout output
+  printf ("   x = %i\n", x);
+  printf ("f(x) = %i\n", y);
+  return EXIT_SUCCESS;
+}
+
+int32_t
+calculate_y (int32_t x, int32_t *y)
+{
+  int32_t  status = 0;
+  int32_t  sockfd;
+  int32_t  msg;
+  uint32_t recv_size;
+  struct   sockaddr_in addr;
+
+  if (NULL == y) {
+    status = -1;
+    fprintf (stderr, "  unable to perform calculate_y (): y has to be pre-allocated\n");
+    return status;
+  }
+  msg = htonl (x);
+
+  addr.sin_family = AF_INET;
+  addr.sin_port   = htons ((uint8_t)DEFAULT_SERVER_PORT);
+  status = inet_pton (AF_INET, DEFAULT_SERVER_ADDR, &addr.sin_addr);
+  if (1 > status) {
+    status = -1;
+    fprintf (stderr, "  unable to perform calculate_y (): inet_pton returned: %s\n", strerror (errno));
+    return status; // return -1 when address invalid
+  }
+
+  // prepare socket and perform bind
+  sockfd = socket (PF_INET, SOCK_DGRAM, 0);
+  if (-1 == status) {
+    fprintf (stderr, "  unable to perform calculate_y (): socket returned: %s\n", strerror (errno));
+    return status;
+  }
+  status = bind (sockfd, (struct sockaddr *)&addr, sizeof (addr));
+  if (-1 == status) {
+    fprintf (stderr, "  unable to perform calculate_y (): bind returned: %s\n", strerror (errno));
+    return status;
+  }
+
+  // handle communication
+  status = sendto (sockfd, (uint8_t *)&msg, sizeof (int32_t), 0, (struct sockaddr *)&addr, sizeof (addr));
+  if (-1 == status) {
+    fprintf (stderr, "  unable to perform calculate_y (): sendto returned: %s\n", strerror (errno));
+    return status;
+  }
+  status = recvfrom (sockfd, (uint8_t *)&msg, sizeof (int32_t), 0, (struct sockaddr *)&addr, &recv_size);
+  if (-1 == status) {
+    fprintf (stderr, "  unable to perform calculate_y (): recvfrom returned: %s\n", strerror (errno));
+    return status;
+  }
+
+  // return with success
+  *y = ntohl (msg);
+  status = 0;
+  return status;
+}
